d19698408c6a34372b3248803c7dd53210300ffb
Esteban Principe <40151035+Miunmn@users.noreply.github.com>
2020-11-13 14:18:28 -0500

    Firstcommit

---
+++ POO2_Project/cmake-build-debug/CMakeFiles/POO2_Project.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POO2_Project.dir/main.cpp.obj"
+  "POO2_Project.exe"
+  "POO2_Project.exe.manifest"
+  "POO2_Project.pdb"
+  "libPOO2_Project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POO2_Project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
